1c90a948712bdae1c631c865d19a0527d2f9005d
EdViTeR <maxpivnenko@mail.ru>
2022-06-20 11:46:58 +0300

    123

--- applicant/upload.php
+++ applicant/upload.php
@@ -28,7 +28,7 @@ if (isset($_FILES['image'])) {
     // Выведем название ошибки
     die($outputMessage);
   } else {
-      $folder="/OpenServer/domains/edu.rsukosygin.ru/files/images/user_images/";
+      $folder="/OpenServer/domains/edu.rsukosygin.com/files/images/user_images/";
       $way = "/files/images/user_images/" . $fileName;
       move_uploaded_file($fileTmpName, $folder.$fileName);
       save_user_images($dbo, $way, $_SESSION['user']['id']);
