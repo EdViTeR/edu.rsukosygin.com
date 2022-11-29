7b9c5da2b633d204458afb43067cfadf5e322933
EdViTeR <maxpivnenko@mail.ru>
2022-11-29 14:52:31 +0300

    Отзывы от экспертов

--- reviews.php
+++ reviews.php
@@ -26,36 +26,24 @@
                     <div class="mb-4 p-5 bg-body rounded shadow-sm">
                         <h6 class="border-bottom pb-2 mb-0">Отзывы и рекомендации по оцениванию заявок онлайн-курсов</h6><br>
                         <form method="POST" action="check_order.php?kurs_id=<? echo $kurs_id; ?>">
-                            <label for="structure" type="text"  class="form-label"><b>Удобность</b> использования платформы (от 1 до 10)</label>
+                            <label for="structure" type="text"  class="form-label">Использование платформы удобно и интуитивно понятно (от 1 до 10)</label>
                             <input name="structure" min='0' max='10' type="number" class="form-control" id="firstName" placeholder="Введите количество баллов от 0 до 10" value="" required="true">
                             </br>
-                            <label for="podhod" class="form-label">Достаточное <b>количество информации</b> от разработчиков онлайн-курсов (от 1 до 10)</label>
+                            <label for="podhod" class="form-label">Достаточное количество информации от разработчиков онлайн-курсов. Запрашиваемое количество информации полностью отражает цельность курса (от 1 до 10)</label>
                             <input name="podhod"  min='0' max='10' type="number" class="form-control" id="firstName" placeholder="Введите количество баллов от 0 до 10" value="" required="true">
                             </br>
-                            <label for="purpose" class="form-label"><b>Критерии</b> оценивания сформированы в соответствии с необходимыми параметрами (от 1 до 10)
+                            <label for="purpose" class="form-label">Критерии оценивания сформированы коректно и полностью удовлетворяют необходимым требованиям оценки (от 1 до 10)
                             </label>
                             <input name="purpose" min='0' max='10' type="number" class="form-control" id="firstName" placeholder="Введите количество баллов от 0 до 10" value="" required="true">
                             </br>
-                            <label for="technology" class="form-label">Оставьте свой отзыв о работе платформы. Также подробно укажите пожелания для улучшения качества сервиса</label>
-                            <input name="technology" min='0' max='15' type="number" class="form-control" id="firstName" placeholder="Введите количество баллов от 0 до 15" value="" required="true">
+                            <label for="technology" class="form-label">Оставьте свой отзыв о работе платформы и взаимодействия с ней. Подробно укажите пожелания для улучшения качества работы сервиса и процесса разработки онлайн-курсов.</label>
+                            <textarea type="text" name="data" class="form-control"  cols="100" rows="7" placeholder="Введите текст"></textarea>
                             </br>
                             <button class="w-100 btn btn-outline-primary btn-lg">Отправить</button>
                         </form>
                     </div>
                 </div>
-                <div class="col-lg-4">
-                    <div class="p-5 bg-white border rounded-3">
-                        <!-- <img src="/images/я.jpg" alt="Письма мастера дзен" width="160" height="160"> -->
-                        <img class="logo_img" src="images/Logo_circl_original.png" width="100" height="100">
-                        </br></br>
-                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                        </svg>
 
-                        <p>Данная страница создана для улучшения качества оценивания заявок онлайн-курсов РГУ им. А.Н. Косыгина.</p><hr>
-                        <p><b>Просим Вас</b> оценить работу сервиса и указать недоработки или положительные моменты работы платформы.</p>
-                    </div>
-                </div>
             </div>
         </div>
     
