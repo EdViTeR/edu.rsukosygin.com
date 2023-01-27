6b13600bcbb53b66aa66c19901af1874d9e7f320
EdViTeR <maxpivnenko@mail.ru>
2023-01-27 15:00:09 +0300

    Готовый отзовик

--- reviews.php
+++ reviews.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+
+?>
 <!doctype html>
 <html lang="ru">
     <head>
@@ -24,23 +28,27 @@
             <div class="row">
                 <div class="col-lg-8">
                     <div class="mb-4 p-5 bg-body rounded shadow-sm">
-                        <h6 class="border-bottom pb-2 mb-0">Отзывы и рекомендации по оцениванию заявок онлайн-курсов</h6><br>
-                        <form method="POST" action="check_order.php?kurs_id=<? echo $kurs_id; ?>">
-                            <label for="structure" type="text"  class="form-label">Использование платформы удобно и интуитивно понятно (от 1 до 10)</label>
-                            <input name="structure" min='0' max='10' type="number" class="form-control" id="firstName" placeholder="Введите количество баллов от 0 до 10" value="" required="true">
-                            </br>
-                            <label for="podhod" class="form-label">Достаточное количество информации от разработчиков онлайн-курсов. Запрашиваемое количество информации полностью отражает цельность курса (от 1 до 10)</label>
-                            <input name="podhod"  min='0' max='10' type="number" class="form-control" id="firstName" placeholder="Введите количество баллов от 0 до 10" value="" required="true">
-                            </br>
-                            <label for="purpose" class="form-label">Критерии оценивания сформированы коректно и полностью удовлетворяют необходимым требованиям оценки (от 1 до 10)
-                            </label>
-                            <input name="purpose" min='0' max='10' type="number" class="form-control" id="firstName" placeholder="Введите количество баллов от 0 до 10" value="" required="true">
-                            </br>
-                            <label for="technology" class="form-label">Оставьте свой отзыв о работе платформы и взаимодействия с ней. Подробно укажите пожелания для улучшения качества работы сервиса и процесса разработки онлайн-курсов.</label>
-                            <textarea type="text" name="data" class="form-control"  cols="100" rows="7" placeholder="Введите текст"></textarea>
+                        <h5 class="border-bottom pb-2 mb-0">Отзывы и пожелания</h5><br>
+                        <form method="POST" action="check_reviews.php">
+                            <label for="technology" class="form-label">Оставьте свой отзыв о работе платформы и взаимодействия с ней. Подробно укажите пожелания для улучшения качества работы сервиса и процесса разработки онлайн-курсов.</label></br></br>
+                            <textarea type="text" name="review" class="form-control"  cols="100" rows="7" placeholder="Введите текст" required></textarea>
                             </br>
                             <button class="w-100 btn btn-outline-primary btn-lg">Отправить</button>
                         </form>
+                        <?php
+                            if ($_SESSION['access']) {
+                                echo '</br><style type="text/css">
+                                        b { 
+                                            color: green;
+                                            margin-left: 200px;
+                                        }
+                                    </style>
+                                <b>' . $_SESSION["access"] . '</b></br></br>
+                                <a href="logout.php" class="w-100 btn btn-outline-success btn-lg">Выйти</a>';
+                            } else {
+                                echo '</br>';
+                            }
+                        ?> 
                     </div>
                 </div>
 
@@ -50,13 +58,6 @@
         <!-- Подвал -->
         <div class="container">
             <footer class="py-3 my-4">
-                <!-- <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
-                </ul> -->
                 <p class="text-center text-muted border-top pt-3 ">&copy; 2022 РГУ им. А.Н. Косыгина</p>
             </footer>
         </div>
