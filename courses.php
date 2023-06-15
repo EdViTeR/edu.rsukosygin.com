4e858f1f51a9a6243fd65fd532d5d6d47f3ec1ed
EdViTeR <maxpivnenko@mail.ru>
2023-06-15 10:25:12 +0300

    верстка

---
+++ courses.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="index_style.css">
+	<title>Онлайн-курсы</title>
+</head>
+<body>
+
+<header class="header">
+	<div class="container">
+		<div class="header__inner">
+			<div class="header__logo">
+				<a class="header__link" href="index.php">Главная</a>
+				<a class="header__link active" href="courses.php">Курсы</a>
+				<a class="header__link" href="news.php">Новости</a>
+			</div>
+			<nav class="nav__link">
+				<a class="nav__link__lk" href="">Личный кабинет</a>
+				<a class="nav__link__edu" href="">EDU</a>
+			</nav>
+		</div>
+	</div>
+</header>
+
+<div class="intro">
+	<div class="container">
+		<div class="intro__logo">
+			<img class="logo__img" src="images/index_img/Logo_icon.svg">
+    		<img class="logo__img__text" src="images/index_img/Logo_text.svg">
+		</div>
+	</div>
+</div>
+<section class="course_all">
+	<div class="head__all__info">
+		<div class="head__info">
+			<h1 class="head__info__title">ОНЛАЙН-КУРСЫ</h1>
+		</div>
+		<div class="head__info__title__text">
+			<h2 class="head__info__course__text">Университет Косыгина активно участвует в создании и продвижении доступного и качественного образования, с использованием дистанционных технологий.<p>Представленные в разделе курсы относятся к дисциплинам «свободного модуля». Программы курсов разработаны ведущими преподавателями университета — экспертами в своей области.</h2>
+		</div>
+	</div>
+
+	<div class="courses__all__cards">
+		<div class="block__three__cards">
+			<div class="course__block">
+				<div class="course__photo">
+					
+				</div>
+				<div class="course__name">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<footer class="footer">
+	123123
+</footer>
+</body>
+</html>
