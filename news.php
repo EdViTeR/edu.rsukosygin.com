98ac6a751618e71e18bea08f0ba487dc5bc960fe
EdViTeR <maxpivnenko@mail.ru>
2023-06-16 17:53:54 +0300

    cho-to

---
+++ news.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="index_style.css">
+	<title>Онлайн-курсы</title>
+</head>
+<body>
+
+<header class="header">
+	<div class="container">
+		<div class="header__inner">
+			<div class="header__logo">
+				<a class="header__link" href="index.php">Главная</a>
+				<a class="header__link" href="courses.php">Курсы</a>
+				<a class="header__link active" href="news.php">Новости</a>
+			</div>
+			<nav class="nav__link">
+				<a class="nav__link__lk" href="">Личный кабинет</a>
+				<a class="nav__link__edu" href="">EDU</a>
+			</nav>
+		</div>
+	</div>
+</header>
+
+<div class="intro">
+	<div class="container">
+		<div class="intro__logo">
+			<img class="logo__img" src="images/index_img/Logo_icon.svg">
+    		<img class="logo__img__text" src="images/index_img/Logo_text.svg">
+		</div>
+	</div>
+</div>
+<footer class="footer">
+	123123
+</footer>
+</body>
+</html>
