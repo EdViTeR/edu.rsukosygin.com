b5829842ebdaee8e2773ea5ee7053b0542e72fc0
SergoAlexeev <sergoalexeev@gmail.com>
2023-11-04 15:16:46 +0300

    index-1

--- courses.php
+++ courses.php
@@ -19,7 +19,6 @@ $kurses = kurses_for_index($dbo);
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </head>
 <body>
-
 <header class="header">
 	<div class="container">
 		<div class="header__inner">
