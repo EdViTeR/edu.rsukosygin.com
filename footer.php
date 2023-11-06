65413a6f9cb453c35efeca48b85c93898da7271c
SergoAlexeev <sergoalexeev@gmail.com>
2023-11-07 01:20:52 +0300

    fix

--- footer.php
+++ footer.php
@@ -2,6 +2,6 @@
 	<div class="f-box">
 		<a href="https://rguk.ru/"><img class="f-box__item" src="svg\logotype-invers.svg" alt=""></a>
 		<p>&#169 2023 ФГБОУ ВО РГУ им. А.Н. Косыгина<br>(Технологии. Дизайн. Искусство)</p>
-		<a class="link" href="https://t.me/sergoalexeev">Дизайн сайта</a>
+		<a class="link" href="https://t.me/sergoalexeev">Дизайн</a>
 	</div>
 </footer>
