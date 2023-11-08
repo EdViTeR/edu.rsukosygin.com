a08ed90f105ced530c99b65d018ef14f26f7bf0e
SergoAlexeev <sergoalexeev@gmail.com>
2023-11-08 12:12:28 +0300

    Добавлена страница Новости, исправлены расположение блоков описания курсов. +++ мелкие исправления кода.

--- item.php
+++ item.php
@@ -1,52 +1,53 @@
 <?php
 require_once 'database/connect_db.php';
 require_once 'database/databaseInfo.php';
-	$id = $_GET['id'];
-	$news_item = getNewsItem($dbo, $id);
-	$result = mb_substr($news_item['date'], 0, 10);
-	list($year, $month, $day) = explode("-", $result);
-	switch ($month) {
-		case '01':
-			$month = 'Января';
-			break;
-		case '02':
-			$month = 'Февраля';
-			break;
-		case '03':
-			$month = 'Марта';
-			break;
-		case '04':
-			$month = 'Апреля';
-			break;
-		case '05':
-			$month = 'Мая';
-			break;
-		case '06':
-			$month = 'Июня';
-			break;
-		case '07':
-			$month = 'Июля';
-			break;
-		case '08':
-			$month = 'Августа';
-			break;
-		case '09':
-			$month = 'Сентября';
-			break;
-		case '10':
-			$month = 'Октября';
-			break;
-		case '11':
-			$month = 'Ноября';
-			break;
-		case '12':
-			$month = 'Декабря';
-			break;
-	}
-	$date = $day . ' ' . $month . ' ' . $year;
+$id = $_GET['id'];
+$news_item = getNewsItem($dbo, $id);
+$result = mb_substr($news_item['date'], 0, 10);
+list($year, $month, $day) = explode("-", $result);
+switch ($month) {
+	case '01':
+		$month = 'Января';
+		break;
+	case '02':
+		$month = 'Февраля';
+		break;
+	case '03':
+		$month = 'Марта';
+		break;
+	case '04':
+		$month = 'Апреля';
+		break;
+	case '05':
+		$month = 'Мая';
+		break;
+	case '06':
+		$month = 'Июня';
+		break;
+	case '07':
+		$month = 'Июля';
+		break;
+	case '08':
+		$month = 'Августа';
+		break;
+	case '09':
+		$month = 'Сентября';
+		break;
+	case '10':
+		$month = 'Октября';
+		break;
+	case '11':
+		$month = 'Ноября';
+		break;
+	case '12':
+		$month = 'Декабря';
+		break;
+}
+$date = $day . ' ' . $month . ' ' . $year;
 ?>
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -58,6 +59,7 @@ require_once 'database/databaseInfo.php';
 	<link type="image/x-icon" href="images/favicon.ico" rel="shortcut icon">
 	<link type="Image/x-icon" href="images/favicon.ico" rel="icon">
 </head>
+
 <body>
 	<header class="header">
 		<ul class="nav navbar justify-content-center">
@@ -76,30 +78,32 @@ require_once 'database/databaseInfo.php';
 			<a class="my-btn my-btn-primary" href="https://edu.rguk.ru/login/index.php">EDU</a>
 		</div>
 	</header>
+
 	<main class="content">
-		<div class="logotype">
-			<img class="logotype__icon" src="images/index_img/Logo_icon.svg">
-			<img class="logotype__text" src="images/index_img/Logo_text.svg">
+		<div class="news-header">
+			<div class="news-header__wrap">
+				<div class="news-header__info">
+					<div class="news__date"><?php echo $date; ?></div>
+				</div>
+				<h1 class="news-header__title"><?php echo $news_item['name']; ?></h1>
+			</div>
+			<?php
+			echo '<img class="news-header__img" src="images/news/news_photo/news__photo' . $news_item['id'] . '.svg" >';
+			?>
 		</div>
-		<div class="box box--size_xl">
-			<h1>Новости онлайн-образования</h1>
+		<div class="box"></div>
+		<div class="box news">
+			<p class="news__subtitle"><?php echo $news_item['title']; ?></p>
+			<p class="news__text"><?php echo $news_item['text']; ?></p>
 		</div>
-<section class="course_all">
-	<div class="news__item">
-		<?php 
-			echo '<img src="images/news/news_photo/news__photo' . $news_item['id'] . '.svg" class="news__photo"></img>';
-		?>
-		<div class="news__header"><?php echo $news_item['name']; ?></div>
-		<div class="news__date"><?php echo $date; ?></div>
-		<div class="news__title"><?php echo $news_item['title']; ?></div>
-		<div class="news__text"><?php echo $news_item['text']; ?></div>
-	</div>
-	<div class="button__link__item">
-		<a class="back__button__link__item" href="news.php">Вернуться</a>
-	</div>
-</section>
-<?php 
+		<div class="box">
+			<a class="my-btn my-btn-outline" href="news.php">Вернуться</a>
+		</div>
+		<div class="box"></div>
+	</main>
+	<?php
 	include('footer.php');
-?>
+	?>
 </body>
+
 </html>
